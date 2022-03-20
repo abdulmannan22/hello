c713d82bb032fdcdaefda1c8d2a428961cf4f6c6
abdulmannan22 <101967085+abdulmannan22@users.noreply.github.com>
2022-03-20 12:49:38 +0500

    Add files via upload
    ok

---
+++ main.dart
@@ -0,0 +1,69 @@
+void main() {
+  var a =
+      "                      ============================                                     ";
+  print(a);
+
+  var a1 =
+      "                       STUDENT NAME : ABDUL MANNAN                                 ";
+  print(a1);
+
+  var a2 =
+      "                       ==========================                                     ";
+  print(a2);
+
+  var a3 = "                   ///QUESTION NO 4///                          ";
+  print(a3);
+
+  var now = ['apple', 'banana', 'mango', 'orange', 'strawberry'];
+  for (var x in now) {
+    print(x);
+  }
+
+  var a5 =
+      "                ==========================                                     ";
+  print(a5);
+
+  var a4 = "                   ///QUESTION NO 5///                          ";
+  print(a4);
+  var num = 7;
+  for (var a = 2; a <= 15; a++) {
+    print("${num} x ${a} = ${num * a} ");
+  }
+
+  var a10 =
+      "                ==========================                                     ";
+  print(a10);
+
+  var a11 = "                   ///QUESTION NO 1///                          ";
+  print(a11);
+
+  var words = {'MANNAN', 'MANNAN', 'MANNAN', 'FARAZ', 'FARAZ', 'FARAZ'};
+  words.remove(1);
+  print(words);
+
+  var a12 =
+      "                    _______________________                             ";
+  print(a12);
+
+  var a13 = "                   ///QUESTION NO 11///                          ";
+  print(a13);
+
+  var words1 = {"ZOHAIB", "ZOHAIB", "ZOHAIB", 'HARIS', 'HARIS', 'HARIS'};
+  words1.remove(1);
+  print(words1);
+
+  var a14 =
+      "                    _______________________                             ";
+  print(a14);
+
+  var a15 = "                   ///QUESTION NO 10///                          ";
+  print(a15);
+
+  var a16 = ["natsikaP nawaJ"];
+//   var reversed = List.of(a16.reversed);
+// print(reversed);
+var reversed = (a16.reversed);
+print(reversed);
+
+
+}
